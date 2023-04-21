b631887ff217fd6ecb057d14750d6ca617e8eff5
Rayan Mazouz <rayan.mazouz@colorado.edu>
2023-04-20 23:34:24 -0600

    Updating necessary armadillo procedure

--- get_dep.dev.sh
+++ get_dep.dev.sh
@@ -11,18 +11,20 @@ sudo apt-get -y install liblapack-dev
 
 #TODO: add the following as submodules
 
-echo "--------------------------------"
-echo "working on armadillo ..."
-mkdir -p armadillo
-cd armadillo
-wget https://kent.dl.sourceforge.net/project/arma/armadillo-10.4.1.tar.xz
-tar -xvf armadillo-10.4.1.tar.xz --strip-components 1
-rm armadillo-10.4.1.tar.xz
-./configure
-make
-sudo make install
-echo "--------------------------------"
-cd ../
+#if [ ! -d "armadillo" ]; then
+  echo "--------------------------------"
+  echo "working on armadillo ..."
+  mkdir -p armadillo
+  cd armadillo
+  wget https://sourceforge.net/projects/arma/files/armadillo-10.4.1.tar.xz
+  tar -xvf armadillo-10.4.1.tar.xz --strip-components 1
+  rm armadillo-10.4.1.tar.xz
+  ./configure
+  make
+  sudo make install
+  echo "--------------------------------"
+  cd ../
+
 
 sudo apt-get -y install libmatio-dev
 sudo apt-get -y install libginac-dev
